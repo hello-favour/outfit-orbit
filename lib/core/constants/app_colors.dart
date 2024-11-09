d44e9fce01103651e80d6022952bcd1bad795398
Favour <hello.williamsfav@gmail.com>
2024-11-09 11:11:58 +0100

    Initial commit: Created Outfitorbit Flutter project with structured folder setup

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color primaryColor = const Color(0xFFDB3022);
+  static Color hintText = const Color(0xFF2D2D2D);
+  static Color borderColor = const Color(0xFF222222);
+  static Color greyColor = const Color(0xFF9B9B9B);
+  static Color whiteColor = const Color(0xFFFFFFFF);
+  static Color blackColor = const Color(0xFF222222);
+  static Color greenColor = const Color(0xFF2AA952);
+
+  //COLORS
+  static Color color1 = const Color(0xFFE2BB8D);
+  static Color color2 = const Color(0xFFBEA9A9);
+  static Color color3 = const Color(0xFF151867);
+  static Color color4 = const Color(0xFFF6F6F6);
+  static Color color5 = const Color(0xFF020202);
+}
